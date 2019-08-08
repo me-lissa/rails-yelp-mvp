90c0faa505678c80865fb2271f3d75c28b3e054b
Melissa Lobuescher <melissalob@gmail.com>
2019-08-08 11:02:06 +0200

    adding state of exercise 3

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'cleaning database'
+Restaurant.destroy_all
+
+puts 'creating restaurants...'
+
+restaurant_parameters = []
+
+10.times do
+  seed_resto = {
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    category: ['chinese', 'italian', 'japanese', 'french', 'belgian'].sample,
+    phone_number: Faker::PhoneNumber.phone_number
+  }
+
+  restaurant_parameters << seed_resto
+end
+
+restaurants = Restaurant.create(restaurant_parameters)
+
+puts 'finished restaurants...'
+
+puts 'creating reviews...'
+
+review_parameters = []
+
+50.times do
+  seed_review = {
+    content: Faker::TvShows::HowIMetYourMother.quote,
+    rating: [0, 1, 2, 3, 4, 5].sample,
+    restaurant_id: restaurants.sample.id
+  }
+  review_parameters << seed_review
+end
+
+Review.create(review_parameters)
+
+puts 'finished reviews...'
